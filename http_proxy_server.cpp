7ddbc005adf3aafe20d5bf0427dacb82fba5b465
Kelvin Hu <ini.kelvin@gmail.com>
2013-09-09 23:40:15 +0800

    add implementation of proxy server

--- http_proxy_server.cpp
+++ http_proxy_server.cpp
@@ -1,10 +1,12 @@
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/asio.hpp>
 #include "log.h"
 #include "http_proxy_server.h"
 
 HttpProxyServer::HttpProxyServer(const std::string& address, int port)
-    : service_(), signals_(service_), acceptor_(service_) {
+    : service_(), signals_(service_), acceptor_(service_),
+      current_session_(), session_manager_() {
     signals_.add(SIGINT);
     signals_.add(SIGTERM);
     // signals_.add(SIGQUIT); // TODO is this needed?
@@ -34,14 +36,24 @@ void HttpProxyServer::Stop() {
 }
 
 void HttpProxyServer::StartAccept() {
-    // TODO implement me
+    current_session_.reset(new HttpProxySession(service_, session_manager_));
+    acceptor_.async_accept(current_session_->LocalSocket(),
+                           boost::bind(&HttpProxyServer::HandleAccept, this,
+                                       boost::asio::placeholders::error));
 }
 
 void HttpProxyServer::HandleAccept(const boost::system::error_code &e) {
-    // TODO implement me
+    if(!acceptor_.is_open()) {
+        Log::warn("socket is not opened.");
+        return;
+    }
+    if(!e)
+        session_manager_.Start(current_session_);
+
+    StartAccept();
 }
 
 void HttpProxyServer::HandleStop() {
     acceptor_.close();
-    // TODO implement me
+    session_manager_.StopAll();
 }
--- http_proxy_server.h
+++ http_proxy_server.h
@@ -5,6 +5,7 @@
 #include <boost/asio/signal_set.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include "server.h"
+#include "http_proxy_session_manager.h"
 
 
 class HttpProxyServer : public Server {
@@ -24,6 +25,9 @@ private:
     boost::asio::io_service service_;
     boost::asio::signal_set signals_;
     boost::asio::ip::tcp::acceptor acceptor_;
+
+    HttpProxySessionPtr current_session_;
+    HttpProxySessionManager session_manager_;
 };
 
 #endif // HTTP_PROXY_SERVER_H
