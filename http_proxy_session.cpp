52e12293cd4fde1508ea1ba093fddc6a6799eaf0
Kelvin Hu <ini.kelvin@gmail.com>
2013-09-09 23:53:34 +0800

    add partial implementation of session

--- http_proxy_session.cpp
+++ http_proxy_session.cpp
@@ -1,3 +1,5 @@
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
 #include "http_proxy_session.h"
 
 HttpProxySession::HttpProxySession(boost::asio::io_service& local_service,
@@ -19,7 +21,21 @@ boost::asio::ip::tcp::socket& HttpProxySession::RemoteSocket() {
 }
 
 void HttpProxySession::Start() {
+    local_socket_.async_read_some(
+                boost::asio::buffer(buffer_),
+                boost::bind(&HttpProxySession::HandleRead,
+                            shared_from_this(),
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred));
 }
 
 void HttpProxySession::Stop() {
+    local_socket_.close();
+    remote_socket_.close();
+}
+
+void HttpProxySession::HandleRead(const boost::system::error_code &e,
+                                  std::size_t size) {
+    std::cout << buffer_.data() << std::endl;
+    // TODO implement me
 }
--- http_proxy_session.h
+++ http_proxy_session.h
@@ -6,6 +6,7 @@
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/array.hpp>
 
 class HttpProxySessionManager;
 
@@ -23,10 +24,14 @@ public:
     void Stop();
 
 private:
+    void HandleRead(const boost::system::error_code& e, std::size_t size);
+
     boost::asio::io_service remote_service_;
     boost::asio::ip::tcp::socket local_socket_;
     boost::asio::ip::tcp::socket remote_socket_;
     HttpProxySessionManager& manager_;
+
+    boost::array<char, 4096> buffer_;
 };
 
 typedef boost::shared_ptr<HttpProxySession> HttpProxySessionPtr;
