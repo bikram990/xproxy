0620fc527a450ccb3e8179801629bb803448bd77
Kelvin Hu <ini.kelvin@gmail.com>
2013-09-13 18:38:09 +0800

    move template function to header file to make the program pass compilation

--- request_dispatcher.cpp
+++ request_dispatcher.cpp
@@ -1,4 +1,3 @@
-#include "log.h"
 #include "request_dispatcher.h"
 
 RequestDispatcher::RequestDispatcher() : service_() {
@@ -7,37 +6,4 @@ RequestDispatcher::RequestDispatcher() : service_() {
 
 RequestDispatcher::~RequestDispatcher() {
     TRACE_THIS_PTR;
-}
-
-template<typename BufT>
-bool RequestDispatcher::DispatchRequest(std::string host, short port, BufT& buffer) {
-    using namespace boost::asio::ip;
-    try {
-        tcp::resolver resolver(service_);
-        tcp::resolver::query query(host);
-        tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
-        tcp::socket socket(service_);
-        boost::asio::connect(socket, endpoint_iterator);
-
-        for(;;) {
-            boost::array<char, 128> buf;
-            boost::system::error_code error;
-
-            std::size_t length = socket.read_some(boost::asio::buffer(buf), error);
-
-            if (error == boost::asio::error::eof)
-                break; // Connection closed cleanly by peer.
-            else if (error)
-                throw boost::system::system_error(error); // Some other error.
-
-            //std::cout.write(buf.data(), length);
-            // TODO uncomment this later when a suitable buffer is found
-            //buffer << buf;
-            //buffer.write(buf.data(), length);
-        }
-    }
-    catch(std::exception& e) {
-        XERROR << e.what();
-    }
 }
--- request_dispatcher.h
+++ request_dispatcher.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <boost/noncopyable.hpp>
 #include <boost/asio.hpp>
+#include "log.h"
 
 
 class RequestDispatcher : private boost::noncopyable {
@@ -17,5 +18,40 @@ public:
 private:
     boost::asio::io_service service_;
 };
+
+template<typename BufT>
+bool RequestDispatcher::DispatchRequest(std::string host, short port, BufT& buffer) {
+    using namespace boost::asio::ip;
+    try {
+        tcp::resolver resolver(service_);
+        tcp::resolver::query query(host);
+        tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+        tcp::socket socket(service_);
+        boost::asio::connect(socket, endpoint_iterator);
+
+        for(;;) {
+            boost::array<char, 128> buf;
+            boost::system::error_code error;
+
+            std::size_t length = socket.read_some(boost::asio::buffer(buf), error);
+
+            if (error == boost::asio::error::eof)
+                break; // Connection closed cleanly by peer.
+            else if (error)
+                throw boost::system::system_error(error); // Some other error.
+
+            //std::cout.write(buf.data(), length);
+            // TODO uncomment this later when a suitable buffer is found
+            //buffer << buf;
+            //buffer.write(buf.data(), length);
+        }
+    }
+    catch(std::exception& e) {
+        XERROR << e.what();
+        return false;
+    }
+    return true;
+}
 
 #endif // REQUEST_DISPATCHER_H
