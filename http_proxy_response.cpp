c9ef1581962d4d1fcf7bcb372955bc69ff55de71
Kelvin Hu <ini.kelvin@gmail.com>
2013-09-14 23:07:37 +0800

    add response class

---
+++ http_proxy_response.cpp
@@ -0,0 +1,10 @@
+#include "log.h"
+#include "http_proxy_response.h"
+
+HttpProxyResponse::HttpProxyResponse() {
+    TRACE_THIS_PTR;
+}
+
+HttpProxyResponse::~HttpProxyResponse() {
+    TRACE_THIS_PTR;
+}
---
+++ http_proxy_response.h
@@ -0,0 +1,10 @@
+#ifndef HTTP_PROXY_RESPONSE_H
+#define HTTP_PROXY_RESPONSE_H
+
+class HttpProxyResponse {
+public:
+    HttpProxyResponse();
+    ~HttpProxyResponse();
+};
+
+#endif // HTTP_PROXY_RESPONSE_H
