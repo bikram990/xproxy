84bc3f75727c4792b3e373f4f6cfe285e70f65c5
Kelvin Hu <ini.kelvin@gmail.com>
2013-09-20 17:24:33 +0800

    update response headers

--- http_proxy_response.h
+++ http_proxy_response.h
@@ -14,6 +14,24 @@ public:
     std::string& status_line() { return status_line_; }
     boost::array<char, 4096>& body() { return body_; }
 
+    void AddHeader(const std::string& name, const std::string& value) {
+        Header h;
+        h.name = name;
+        h.value = value;
+        headers_.push_back(h);
+    }
+
+    std::string& headers() {
+        header_string_ = "";
+        for(std::vector<Header>::iterator it = headers_.begin(); it != headers_.end(); ++it) {
+            header_string_ += it->name;
+            header_string_ += ": ";
+            header_string_ += it->value;
+            header_string_ += '\n';
+        }
+        return header_string_;
+    }
+
 private:
     struct Header {
         std::string name;
@@ -22,6 +40,7 @@ private:
 
     std::string status_line_;
     std::vector<Header> headers_;
+    std::string header_string_;
     boost::array<char, 4096> body_;
 };
 
