dfe6412cf643242faa9356e10886ed659124139c
Kelvin Hu <ini.kelvin@gmail.com>
2013-09-24 01:43:06 +0800

    do not trace this pointer of header struct, it is meaningless and there will be too much log

--- http_header.h
+++ http_header.h
@@ -1,21 +1,13 @@
 #ifndef HTTP_HEADER_H
 #define HTTP_HEADER_H
 
-#include "log.h"
-
-
 struct HttpHeader {
     std::string name;
     std::string value;
 
     HttpHeader(const std::string& name = "",
                const std::string& value = "")
-        : name(name), value(value) {
-        TRACE_THIS_PTR;
-    }
-    ~HttpHeader() {
-        TRACE_THIS_PTR;
-    }
+        : name(name), value(value) {}
 };
 
 #endif // HTTP_HEADER_H
