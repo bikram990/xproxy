93aed107499f7fcd6b39910db0a4aa2f7a019dad
Kelvin Hu <ini.kelvin@gmail.com>
2013-10-20 18:01:16 +0800

    update https proxy handler, using http client

--- https_proxy_handler.cpp
+++ https_proxy_handler.cpp
@@ -10,24 +10,100 @@ extern ProxyConfiguration g_config;
 
 HttpsProxyHandler::HttpsProxyHandler(HttpProxySession& session,
                                      HttpRequestPtr request)
-    : session_(session), local_socket_(session.LocalSocket()),
-      context_(boost::asio::ssl::context::sslv23),
-      remote_socket_(session.service(), context_),
-      resolver_(session.service()),
-      origin_request_(request) {
-    TRACE_THIS_PTR;
-    remote_socket_.set_verify_mode(boost::asio::ssl::verify_peer);
-    remote_socket_.set_verify_callback(boost::bind(&HttpsProxyHandler::VerifyCertificate, this, _1, _2));
-}
-
-HttpsProxyHandler::~HttpsProxyHandler() {
+    : session_(session), local_ssl_context_(session.LocalSSLContext()),
+      local_ssl_socket_(session.LocalSocket(), local_ssl_context_),
+      remote_ssl_context_(boost::asio::ssl::context::sslv23),
+      origin_request_(request),
+      client_(session.service(), request.get(), &remote_ssl_context_) {
     TRACE_THIS_PTR;
 }
 
 void HttpsProxyHandler::HandleRequest() {
-    XTRACE << "New request received, host: " << origin_request_->host()
+    XTRACE << "Received a HTTPS request, host: " << origin_request_->host()
            << ", port: " << origin_request_->port();
-    ResolveRemote();
+
+    static std::string response("HTTP/1.1 200 Connection Established\r\nProxy-Connection: Keep-Alive\r\n\r\n");
+    boost::asio::async_write(local_ssl_socket_.next_layer(), boost::asio::buffer(response),
+                             boost::bind(&HttpsProxyHandler::OnLocalSSLReplySent,
+                                         boost::static_pointer_cast<HttpsProxyHandler>(shared_from_this()),
+                                         boost::asio::placeholders::error));
+}
+
+void HttpsProxyHandler::OnLocalSSLReplySent(const boost::system::error_code& e) {
+    if(e) {
+        XWARN << "Failed to write SSL OK reply to local socket, message: " << e.message();
+        session_.Terminate();
+        return;
+    }
+
+    local_ssl_socket_.async_handshake(boost::asio::ssl::stream_base::server,
+                                      boost::bind(&HttpsProxyHandler::OnLocalHandshaken,
+                                                  boost::static_pointer_cast<HttpsProxyHandler>(shared_from_this()),
+                                                  boost::asio::placeholders::error));
+}
+
+void HttpsProxyHandler::OnLocalHandshaken(const boost::system::error_code& e) {
+    if(e) {
+        XWARN << "Failed to handshake with local client, message: " << e.message();
+        session_.Terminate();
+        return;
+    }
+
+    boost::asio::streambuf::mutable_buffers_type buf = local_buffer_.prepare(4096); // TODO hard code
+    local_ssl_socket_.async_read_some(buf,
+                                      boost::bind(&HttpsProxyHandler::OnLocalDataReceived,
+                                                  boost::static_pointer_cast<HttpsProxyHandler>(shared_from_this()),
+                                                  boost::asio::placeholders::error,
+                                                  boost::asio::placeholders::bytes_transferred));
+}
+
+void HttpsProxyHandler::OnLocalDataReceived(const boost::system::error_code& e, std::size_t size) {
+    if(e) {
+        XWARN << "Failed to receive data from local socket, message: " << e.message();
+        session_.Terminate();
+        return;
+    }
+
+    local_buffer_.commit(size);
+
+    XDEBUG << "Dump ssl encrypted data from local socket(size:" << size << "):\n"
+           << "--------------------------------------------\n"
+           << boost::asio::buffer_cast<const char *>(local_buffer_.data())
+           << "\n--------------------------------------------";
+
+    // TODO can we build on the original request?
+    // TODO2 we should send the raw data directly, do not do parse and compose work
+    HttpRequest::State result = HttpRequest::BuildRequest(const_cast<char *>(boost::asio::buffer_cast<const char *>(local_buffer_.data())),
+                                                          local_buffer_.size(), *origin_request_);
+
+    if(result != HttpRequest::kComplete) {
+        XWARN << "This request is not complete, continue to read from the ssl socket.";
+        boost::asio::streambuf::mutable_buffers_type buf = local_buffer_.prepare(2048); // TODO hard code
+        local_ssl_socket_.async_read_some(buf,
+                                          boost::bind(&HttpsProxyHandler::OnLocalDataReceived,
+                                                      boost::static_pointer_cast<HttpsProxyHandler>(shared_from_this()),
+                                                      boost::asio::placeholders::error,
+                                                      boost::asio::placeholders::bytes_transferred));
+        return;
+    }
+
+//    if(result == HttpRequest::kIncomplete) {
+//        XWARN << "Not a complete request, but currently partial request is not supported.";
+//        session_.Terminate();
+//        return;
+//    } else if(result == HttpRequest::kBadRequest) {
+//        XWARN << "Bad request: " << local_ssl_socket_.lowest_layer().remote_endpoint().address()
+//              << ":" << local_ssl_socket_.lowest_layer().remote_endpoint().port();
+//        // TODO here we should write a bad request response back
+//        session_.Terminate();
+//        return;
+//    }
+
+    BuildProxyRequest(proxy_request_);
+    client_.host(g_config.GetGAEServerDomain()).port(443).request(&proxy_request_)
+           .AsyncSendRequest(boost::bind(&HttpsProxyHandler::OnResponseReceived,
+                                         boost::static_pointer_cast<HttpsProxyHandler>(shared_from_this()), _1, _2));
+    XTRACE << "Request is sent asynchronously.";
 }
 
 void HttpsProxyHandler::BuildProxyRequest(HttpRequest& request) {
@@ -46,175 +122,33 @@ void HttpsProxyHandler::BuildProxyRequest(HttpRequest& request) {
            .body_length(length);
 }
 
-void HttpsProxyHandler::ResolveRemote() {
-    const std::string host = g_config.GetGAEServerDomain();
-    short port = 443;
-
-    XDEBUG << "Resolving remote address, host: " << host << ", port: " << port;
-
-    boost::asio::ip::tcp::resolver::query query(host, boost::lexical_cast<std::string>(port));
-    boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver_.resolve(query);
-
-    XDEBUG << "Connecting to remote address: " << endpoint_iterator->endpoint().address();
-
-    boost::asio::async_connect(remote_socket_.lowest_layer(), endpoint_iterator,
-                               boost::bind(&HttpsProxyHandler::OnRemoteConnected,
-                                           this,
-                                           boost::asio::placeholders::error));
-}
-
-void HttpsProxyHandler::OnRemoteConnected(const boost::system::error_code& e) {
-    if(e) {
-        XWARN << "Failed to connect to remote server, message: " << e.message();
-        session_.Stop();
+void HttpsProxyHandler::OnResponseReceived(const boost::system::error_code& e, HttpResponse *response) {
+    if(e && e != boost::asio::error::eof) {
+        XWARN << "Failed to send request, message: " << e.message();
+        session_.Terminate();
         return;
     }
 
-    remote_socket_.async_handshake(boost::asio::ssl::stream_base::client,
-                                   boost::bind(&HttpsProxyHandler::OnRemoteHandshaken,
-                                               this,
-                                               boost::asio::placeholders::error));
-}
+    XTRACE << "Response is back, status line: " << response->status_line();
 
-void HttpsProxyHandler::OnRemoteDataSent(const boost::system::error_code& e) {
-    if(e) {
-        XWARN << "Failed to write request to remote server, message: " << e.message();
-        session_.Stop();
-        return;
-    }
-    boost::asio::async_read_until(remote_socket_, remote_buffer_, "\r\n",
-                                  boost::bind(&HttpsProxyHandler::OnRemoteStatusLineReceived,
-                                              this,
-                                              boost::asio::placeholders::error));
-}
-
-void HttpsProxyHandler::OnRemoteStatusLineReceived(const boost::system::error_code& e) {
-    if(e) {
-        XWARN << "Failed to read status line from remote server, message: " << e.message();
-        session_.Stop();
-        return;
-    }
-
-    // As async_read_until may return more data beyond the delimiter, so we only process the status line
-    std::istream response(&remote_buffer_);
-    std::getline(response, response_.status_line());
-    response_.status_line() += '\n'; // append the missing newline character
-
-    XDEBUG << "Status line from remote server: " << response_.status_line();
-
-    boost::asio::async_read_until(remote_socket_, remote_buffer_, "\r\n\r\n",
-                                  boost::bind(&HttpsProxyHandler::OnRemoteHeadersReceived,
-                                              this,
-                                              boost::asio::placeholders::error));
-}
-
-void HttpsProxyHandler::OnRemoteHeadersReceived(const boost::system::error_code& e) {
-    if(e) {
-        XWARN << "Failed to read response header from remote server, message: " << e.message();
-        session_.Stop();
-        return;
-    }
-
-    XDEBUG << "Headers from remote server: \n" << boost::asio::buffer_cast<const char *>(remote_buffer_.data());
-
-    std::istream response(&remote_buffer_);
-    std::string header;
-    std::size_t body_len = 0;
-    while(std::getline(response, header)) {
-        if(header == "\r") { // there is no more headers
-            XDEBUG << "no more headers";
-            break;
-        }
-
-        std::string::size_type sep_idx = header.find(": ");
-        if(sep_idx == std::string::npos) {
-            XWARN << "Invalid header: " << header;
-            continue;
-        }
-
-        std::string name = header.substr(0, sep_idx);
-        std::string value = header.substr(sep_idx + 2, header.length() - 1 - name.length() - 2); // remove the last \r
-        response_.AddHeader(name, value);
-
-        XTRACE << "header name: " << name << ", value: " << value;
-
-        if(name == "Content-Length") {
-            boost::algorithm::trim(value);
-            body_len = boost::lexical_cast<std::size_t>(value);
-        }
-    }
-
-    if(body_len <= 0) {
-        XERROR << "The proxy server returns no body.";
-        //boost::system::error_code ec;
-        //remote_socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
-        return;
-    }
-
-    response_.body_lenth(body_len);
-    boost::asio::async_read(remote_socket_, remote_buffer_, boost::asio::transfer_at_least(1),
-                            boost::bind(&HttpsProxyHandler::OnRemoteBodyReceived,
-                                        this,
-                                        boost::asio::placeholders::error));
-}
-
-void HttpsProxyHandler::OnRemoteBodyReceived(const boost::system::error_code& e) {
-    if(e) {
-        if(e == boost::asio::error::eof)
-            XDEBUG << "The remote peer closed the connection.";
-        else {
-            XWARN << "Failed to read body from remote server, message: " << e.message();
-            session_.Stop();
-            return;
-        }
-    }
-
-    std::size_t read = remote_buffer_.size();
-    std::size_t copied = boost::asio::buffer_copy(boost::asio::buffer(response_.body()), remote_buffer_.data());
-
-    XDEBUG << "Body from remote server, size: " << read
-           << ", content:\n" << boost::asio::buffer_cast<const char *>(remote_buffer_.data());
-    XDEBUG << "Body copied from raw stream to response, copied: " << copied
-           << ", response body size: " << response_.body().size();
-
-    boost::asio::async_write(local_socket_, boost::asio::buffer(response_.body(), copied),
+    boost::asio::async_write(local_ssl_socket_, response->body(),
                              boost::bind(&HttpsProxyHandler::OnLocalDataSent,
-                                         this,
-                                         boost::asio::placeholders::error, read >= response_.body_length()));
-    if(copied < read) {
-        // TODO the response's body buffer is less than read content, try write again
-    }
-
-    remote_buffer_.consume(read); // the read bytes are consumed
-
-    if(read < response_.body_length()) { // there is more content
-        response_.body_lenth(response_.body_length() - read);
-        boost::asio::async_read(remote_socket_, remote_buffer_, boost::asio::transfer_at_least(1/*body_len*/),
-                                boost::bind(&HttpsProxyHandler::OnRemoteBodyReceived,
-                                            this,
-                                            boost::asio::placeholders::error));
-    } else {
-        //boost::system::error_code ec;
-        //remote_socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
-    }
+                                         boost::static_pointer_cast<HttpsProxyHandler>(shared_from_this()),
+                                         boost::asio::placeholders::error));
 }
 
-void HttpsProxyHandler::OnLocalDataSent(const boost::system::error_code& e,
-                                        bool finished) {
+void HttpsProxyHandler::OnLocalDataSent(const boost::system::error_code& e) {
     if(e) {
         XWARN << "Failed to write response to local socket, message: " << e.message();
-        session_.Stop();
+        session_.Terminate();
         return;
     }
 
     XDEBUG << "Content written to local socket.";
 
-    if(!finished)
-        return;
-
     if(!e) {
         boost::system::error_code ec;
-        local_socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+        //local_socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
         //remote_socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
     }
 
@@ -222,32 +156,4 @@ void HttpsProxyHandler::OnLocalDataSent(const boost::system::error_code& e,
         //manager_.Stop(shared_from_this());
         session_.Terminate();
     }
-}
-
-bool HttpsProxyHandler::VerifyCertificate(bool pre_verified, boost::asio::ssl::verify_context& ctx) {
-    // TODO enhance this function
-    char subject_name[256];
-    X509 *cert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
-    X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
-    XDEBUG << "Verify remote certificate, subject name: " << subject_name
-           << ", pre_verified value: " << pre_verified;
-
-    return true;
-}
-
-void HttpsProxyHandler::OnRemoteHandshaken(const boost::system::error_code& e) {
-    if(e) {
-        XWARN << "Failed at handshake step, message: " << e.message();
-        session_.Stop();
-        return;
-    }
-
-    BuildProxyRequest(proxy_request_);
-
-    XTRACE << boost::asio::buffer_cast<const char *>(proxy_request_.OutboundBuffer().data());
-
-    boost::asio::async_write(remote_socket_, proxy_request_.OutboundBuffer(),
-                             boost::bind(&HttpsProxyHandler::OnRemoteDataSent,
-                                         this,
-                                         boost::asio::placeholders::error));
 }
--- https_proxy_handler.h
+++ https_proxy_handler.h
@@ -1,10 +1,7 @@
 #ifndef HTTPS_PROXY_HANDLER_H
 #define HTTPS_PROXY_HANDLER_H
 
-#include <boost/asio.hpp>
-#include <boost/asio/ssl.hpp>
-#include "http_request.h"
-#include "http_response.h"
+#include "http_client.h"
 #include "request_handler.h"
 
 
@@ -12,37 +9,32 @@ class HttpProxySession;
 
 class HttpsProxyHandler : public RequestHandler {
 public:
-    typedef boost::asio::ssl::stream<boost::asio::ip::tcp::socket> ssl_socket;
+    typedef boost::asio::ssl::stream<boost::asio::ip::tcp::socket&> ssl_socket_ref;
+
     HttpsProxyHandler(HttpProxySession& session, HttpRequestPtr request);
-    ~HttpsProxyHandler();
+    ~HttpsProxyHandler() { TRACE_THIS_PTR; }
 
     void HandleRequest();
 
 private:
+    void OnLocalSSLReplySent(const boost::system::error_code& e);
+    void OnLocalHandshaken(const boost::system::error_code& e);
+    void OnLocalDataReceived(const boost::system::error_code& e, std::size_t size);
     void BuildProxyRequest(HttpRequest& request);
-    void ResolveRemote();
-    void OnRemoteConnected(const boost::system::error_code& e);
-    void OnRemoteDataSent(const boost::system::error_code& e);
-    void OnRemoteStatusLineReceived(const boost::system::error_code& e);
-    void OnRemoteHeadersReceived(const boost::system::error_code& e);
-    void OnRemoteBodyReceived(const boost::system::error_code& e);
-    void OnLocalDataSent(const boost::system::error_code& e, bool finished);
-
-    bool VerifyCertificate(bool pre_verified, boost::asio::ssl::verify_context& ctx);
-    void OnRemoteHandshaken(const boost::system::error_code& e);
+    void OnResponseReceived(const boost::system::error_code& e, HttpResponse *response = NULL);
+    void OnLocalDataSent(const boost::system::error_code& e);
 
     HttpProxySession& session_;
+    boost::asio::ssl::context& local_ssl_context_;
+    ssl_socket_ref local_ssl_socket_; // wrap the local socket
+    boost::asio::ssl::context remote_ssl_context_;
 
-    boost::asio::ip::tcp::socket& local_socket_;
-    boost::asio::ssl::context context_;
-    ssl_socket remote_socket_; // TODO close the socket?
-    boost::asio::ip::tcp::resolver resolver_;
-
-    boost::asio::streambuf remote_buffer_;
+    boost::asio::streambuf local_buffer_;
 
     HttpRequestPtr origin_request_;
     HttpRequest proxy_request_;
-    HttpResponse response_;
+
+    HttpClient client_;
 };
 
 #endif // HTTPS_PROXY_HANDLER_H
