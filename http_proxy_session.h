bac5e30e7915d06564c43fd45e6a0809c725efd8
Kelvin Hu <ini.kelvin@gmail.com>
2013-10-25 01:16:03 +0800

    remove unused root ca ssl context and unused headers

--- http_proxy_session.h
+++ http_proxy_session.h
@@ -1,15 +1,7 @@
 #ifndef HTTP_PROXY_SESSION_H
 #define HTTP_PROXY_SESSION_H
 
-#include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include <boost/asio/ssl.hpp>
-#include <boost/enable_shared_from_this.hpp>
-#include <boost/noncopyable.hpp>
-#include <boost/shared_ptr.hpp>
-#include "http_direct_handler.h"
-#include "http_request.h"
-#include "http_response.h"
 #include "request_handler.h"
 
 
@@ -20,13 +12,10 @@ class HttpProxySession
           private boost::noncopyable {
 public:
     HttpProxySession(boost::asio::io_service& service,
-                     boost::asio::ssl::context& context,
                      HttpProxySessionManager& manager);
     ~HttpProxySession();
     boost::asio::io_service& service() { return service_; }
     boost::asio::ip::tcp::socket& LocalSocket() { return local_socket_; }
-    //boost::asio::ip::tcp::socket& RemoteSocket() { return remote_socket_; }
-    boost::asio::ssl::context& LocalSSLContext() { return local_ssl_context_; }
     void Start();
     void Stop();
     void Terminate();
@@ -35,9 +24,7 @@ private:
     void OnRequestReceived(const boost::system::error_code& e, std::size_t size);
 
     boost::asio::io_service& service_;
-    boost::asio::ssl::context& local_ssl_context_;
     boost::asio::ip::tcp::socket local_socket_;
-    //boost::asio::ip::tcp::socket remote_socket_;
     HttpProxySessionManager& manager_;
 
     boost::array<char, 4096> local_buffer_;
