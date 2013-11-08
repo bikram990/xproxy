1204f4e86d282544979e46591335a0be7999fb48
Kelvin Hu <ini.kelvin@gmail.com>
2013-11-08 20:37:35 +0800

    make manager's constructor private

--- http_proxy_session_manager.h
+++ http_proxy_session_manager.h
@@ -2,20 +2,21 @@
 #define HTTP_PROXY_SESSION_MANAGER_H
 
 #include <set>
-#include <boost/noncopyable.hpp>
 #include "http_proxy_session.h"
 
+class ProxyServer;
 
 class HttpProxySessionManager : private boost::noncopyable {
+    friend class ProxyServer;
 public:
-    HttpProxySessionManager();
-    ~HttpProxySessionManager();
-
     void Start(HttpProxySession::Ptr session);
     void Stop(HttpProxySession::Ptr session);
     void StopAll();
 
 private:
+    HttpProxySessionManager();
+    ~HttpProxySessionManager();
+
     std::set<HttpProxySession::Ptr> sessions_;
 };
 
