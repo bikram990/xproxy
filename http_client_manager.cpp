055549f7c240b33a9f5b0e16304e705678949a3b
Kelvin Hu <ini.kelvin@gmail.com>
2013-11-12 21:24:04 +0800

    update logging for http client caching

--- http_client_manager.cpp
+++ http_client_manager.cpp
@@ -36,6 +36,8 @@ inline HttpClient::Ptr HttpClientManager::get(HttpRequest *request) {
         cit = cache_.find(key);
         if(cit == cache_.end()) {
             HttpClient::Ptr client(HttpClient::Create(*fetch_service_));
+            XDEBUG << "No http client for [" << request->host() << ":" << request->port()
+                   << "] found, new one [id: " << client->id() << "] created.";
             CacheValue value;
             value.clients->push_back(client);
             cache_.insert(std::make_pair(key, value));
@@ -49,12 +51,14 @@ inline HttpClient::Ptr HttpClientManager::get(HttpRequest *request) {
     value_iterator_type vit = std::find_if(value.clients->begin(), value.clients->end(), AvailabilitySearcher());
     if(vit != value.clients->end()) {
         XDEBUG << "Cached http client " << "[id: " << (*vit)->id() << "]"
-               << " for host: " << request->host() << ", port: " << request->port() << " found.";
+               << " for [" << request->host() << ":" << request->port() << "] found.";
         (*vit)->state(HttpClient::kInUse);
         return *vit;
     }
 
     HttpClient::Ptr client(HttpClient::Create(*fetch_service_));
+    XDEBUG << "Http clients for [" << request->host() << ":" << request->port()
+           << "] are all busy, new one [id: " << client->id() << "] created.";
     value.clients->push_back(client);
     client->state(HttpClient::kInUse);
     return client;
