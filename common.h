c86a5b0d1d89f9c0e24c5cbbcb49f91163deb4f2
Kelvin Hu <ini.kelvin@gmail.com>
2013-11-13 17:41:24 +0800

    update common header

--- common.h
+++ common.h
@@ -1,6 +1,9 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+
 #define DECLARE_GENERAL_CONSTRUCTOR(Class) \
     Class() { TRACE_THIS_PTR; }
 
@@ -18,5 +21,18 @@
         delete [] member; \
         member = NULL; \
     }
+
+typedef boost::asio::ip::tcp::socket socket_type;
+typedef boost::asio::ssl::stream<socket_type> ssl_socket_type;
+typedef boost::asio::ssl::stream<socket_type&> ssl_socket_ref_type;
+typedef boost::asio::ssl::context ssl_context_type;
+
+enum Protocol {
+    kHttp, kHttps
+};
+
+enum SSLMode {
+    kClient, kServer
+};
 
 #endif // COMMON_H
