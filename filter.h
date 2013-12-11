bf967461628367285c60b727f28d7f062cc7bccf
Kelvin Hu <ini.kelvin@gmail.com>
2013-12-11 22:38:38 +0800

    new interface: Filter, to filter requests and responses

---
+++ filter.h
@@ -0,0 +1,24 @@
+#ifndef FILTER_H
+#define FILTER_H
+
+class HttpObject;
+
+/**
+ * @brief The Filter class
+ *
+ * Classes inherit from this interface are used to filter requests and responses
+ */
+class Filter {
+public:
+    enum FilterResult {
+        kContinue, // go to next filter
+        kSkip,     // skip all left filters
+        kStop      // stop filtering, indicates there is error occurred
+    };
+
+    virtual ~Filter() {}
+
+    virtual FilterResult process(const HttpObject& object) = 0;
+};
+
+#endif // FILTER_H
