50a7a0fc274229f36e5eae7538cb58e4183e0d8e
Kelvin Hu <ini.kelvin@gmail.com>
2013-12-12 15:16:19 +0800

    new class: HttpChunk

---
+++ http_chunk.h
@@ -0,0 +1,23 @@
+#ifndef HTTP_CHUNK_H
+#define HTTP_CHUNK_H
+
+#include "http_object.h"
+
+class HttpChunk : public HttpObject {
+public:
+    HttpChunk() : HttpObject(), last_(false) {}
+
+    virtual ~HttpChunk() {}
+
+    void SetLast(bool value) { last_ = value; }
+    bool IsLast() const { return last_; }
+
+private:
+    virtual void UpdateByteBuffer() {
+        // TODO do nothing here
+    }
+
+    bool last_; // whether this is the last chunk
+};
+
+#endif // HTTP_CHUNK_H
