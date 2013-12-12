f44c5d2804ddecf4a02326eee6edd97eb7482600
Kelvin Hu <ini.kelvin@gmail.com>
2013-12-12 15:34:25 +0800

    new container class: HttpBody

---
+++ http_body.h
@@ -0,0 +1,22 @@
+#ifndef HTTP_BODY_H
+#define HTTP_BODY_H
+
+#include <vector>
+
+class HttpChunk;
+
+/**
+ * @brief The HttpBody class
+ *
+ * This class is to combine all http chunks together, it is just a container
+ * class, so it does not inherit from HttpObject.
+ */
+class HttpBody {
+public:
+    virtual ~HttpBody() {}
+
+private:
+    std::vector<HttpChunk> chunks_;
+};
+
+#endif // HTTP_BODY_H
