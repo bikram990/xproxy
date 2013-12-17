6c1f77b1d172991bc3ada424641047cbb921d027
Kelvin Hu <ini.kelvin@gmail.com>
2013-12-17 13:48:58 +0800

    update ServerConnectionManager, use key-value container

--- server_connection_manager.cpp
+++ server_connection_manager.cpp
@@ -2,21 +2,43 @@
 #include "server_connection_manager.h"
 
 ConnectionPtr ServerConnectionManager::RequireConnection(const std::string &host, short port) {
+    std::string key(host + ":" + std::to_string(port));
+
     boost::lock_guard<boost::mutex> lock(lock_);
 
-    if(connections_.size() > 0) {
-        ConnectionPtr connection = connections_.front();
-        connections_.pop_front();
+    auto it = connections_.find(key);
+    if(it == connections_.end()) {
+        ConnectionPtr connection(new ServerConnection(fetch_service_));
+        connection->SetRemoteAddress(host, port);
         return connection;
     }
 
-    return ConnectionPtr(new ServerConnection(fetch_service_));
+    value_type& v = it->second;
+    if(v.size() > 0) {
+        ConnectionPtr connection = v.front();
+        v.pop_front();
+        return connection;
+    }
+
+    ConnectionPtr connection(new ServerConnection(fetch_service_));
+    connection->SetRemoteAddress(host, port);
+    return connection;
 }
 
 void ServerConnectionManager::ReleaseConnection(ConnectionPtr connection) {
     if(!connection)
         return;
 
+    std::string key(connection->host() + ":" + std::to_string(connection->port()));
+
     boost::lock_guard<boost::mutex> lock(lock_);
-    connections_.push_front(connection);
+    auto it = connections_.find(key);
+    if(it != connections_.end()) {
+        it->second.push_front(connection);
+        return;
+    }
+
+    value_type v;
+    v.push_front(connection);
+    connections_.insert(std::pair<std::string, value_type>(key, v));
 }
--- server_connection_manager.h
+++ server_connection_manager.h
@@ -2,6 +2,7 @@
 #define SERVER_CONNECTION_MANAGER_H
 
 #include <list>
+#include <map>
 #include <boost/asio.hpp>
 #include <boost/noncopyable.hpp>
 #include <boost/thread/mutex.hpp>
@@ -17,11 +18,14 @@ public:
     void ReleaseConnection(boost::shared_ptr<Connection> connection);
 
 private:
+    typedef std::list<boost::shared_ptr<Connection>> value_type;
+    typedef std::map<std::string, value_type> cache_type;
+
     ServerConnectionManager(boost::asio::io_service& service)
         : fetch_service_(service) {}
 
     boost::asio::io_service& fetch_service_;
-    std::list<boost::shared_ptr<Connection>> connections_;
+    cache_type connections_;
     boost::mutex lock_;
 };
 
