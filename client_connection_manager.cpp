134db6f2f989939f4810e12cbdda2692e12a5f2c
Kelvin Hu <ini.kelvin@gmail.com>
2014-01-03 22:41:51 +0800

    fix crash issue: when using Ctrl-C to stop program, it crashes because invalid virtual function invocation in Connection class

--- client_connection_manager.cpp
+++ client_connection_manager.cpp
@@ -13,7 +13,7 @@ void ClientConnectionManager::stop(ConnectionPtr connection) {
 }
 
 void ClientConnectionManager::StopAll() {
-    std::for_each(connections_.begin(), connections_.end(),
-                  boost::bind(&Connection::stop, _1));
+//    std::for_each(connections_.begin(), connections_.end(),
+//                  boost::bind(&Connection::stop, _1));
     connections_.clear();
 }
