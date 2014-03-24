718cfd90d1b057e90797a482b68480362178ef86
Kelvin Hu <ini.kelvin@gmail.com>
2014-03-24 21:05:35 +0800

    update class HttpMessage

--- http_message.cpp
+++ http_message.cpp
@@ -1,1 +1,68 @@
 #include "http_message.h"
+
+HttpMessage::HttpMessage(http_parser_type type)
+    : completed_(false),
+      HttpParser(type) {}
+
+HttpMessage::~HttpMessage() {}
+
+void HttpMessage::reset() {
+    completed_ = false;
+    current_header_field_.clear();
+    current_header_value_.clear();
+    headers_.reset();
+    if (body_.size())
+        body_.consume(body_.size());
+}
+
+int HttpMessage::OnMessageBegin() {
+    completed_ = false;
+    return 0;
+}
+
+int HttpMessage::OnUrl(const char *at, std::size_t length) {
+    return 0;
+}
+
+int HttpMessage::OnStatus() {
+    return 0;
+}
+
+int HttpMessage::OnHeaderField(const char *at, std::size_t length) {
+    if (!current_header_value_.empty()) {
+        headers_.PushBack(HttpHeader(current_header_field_, current_header_value_));
+        current_header_field_.clear();
+        current_header_value_.clear();
+    }
+
+    current_header_field_.append(at, length);
+
+    return 0;
+}
+
+int HttpMessage::OnHeaderValue(const char *at, std::size_t length) {
+    current_header_value_.append(at, length);
+    return 0;
+}
+
+int HttpMessage::OnHeadersComplete() {
+    if (!current_header_value_.empty()) {
+        headers_.PushBack(HttpHeader(current_header_field_, current_header_value_));
+        current_header_field_.clear();
+        current_header_value_.clear();
+    }
+
+    return 0;
+}
+
+int HttpMessage::OnBody(const char *at, std::size_t length) {
+    boost::asio::buffer_copy(body_.prepare(length),
+                             boost::asio::buffer(at, length));
+    body_.commit(length);
+    return 0;
+}
+
+int HttpMessage::OnMessageComplete() {
+    completed_ = true;
+    return 0;
+}
--- http_message.h
+++ http_message.h
@@ -3,15 +3,48 @@
 
 #include "common.h"
 #include "http_headers.h"
+#include "http_parser.h"
+#include "serializable.h"
 
-class HttpMessage {
+class HttpMessage : public Serializable, public HttpParser {
 public:
-    HttpMessage();
-    virtual ~HttpMessage() {}
+    virtual bool serialize(boost::asio::streambuf& out_buffer) = 0;
 
-    virtual void reset() = 0;
+    virtual void reset();
+
+    virtual int OnMessageBegin();
+
+    // this method should be overridden in HttpRequest
+    virtual int OnUrl(const char *at, std::size_t length);
+
+    // this method should be overridden in HttpResponse
+    virtual int OnStatus();
+
+    virtual int OnHeaderField(const char *at, std::size_t length);
+
+    virtual int OnHeaderValue(const char *at, std::size_t length);
+
+    virtual int OnHeadersComplete();
+
+    virtual int OnBody(const char *at, std::size_t length);
+
+    virtual int OnMessageComplete();
+
+    bool completed() const { return completed_; }
+
+    int MajorVersion() const { return parser_.http_major; }
+
+    int MinorVersion() const { return parser_.http_minor; }
 
 protected:
+    HttpMessage(http_parser_type type);
+    virtual ~HttpMessage();
+
+protected:
+    bool completed_;
+    std::string current_header_field_; // store temp header name/value during parsing
+    std::string current_header_value_;
+
     HttpHeaders headers_;
     boost::asio::streambuf body_;
 
