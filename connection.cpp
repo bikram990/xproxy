8d73c707149e0c75f3ff9565efb4f737cf9ff49e
Kelvin Hu <ini.kelvin@gmail.com>
2014-03-24 21:44:26 +0800

    update interface Connection

--- connection.cpp
+++ connection.cpp
@@ -1,1 +1,34 @@
 #include "connection.h"
+#include "session.h"
+
+Connection::Connection(std::shared_ptr<Session> session,
+                       long timeout,
+                       std::size_t buffer_size)
+    : session_(session),
+      service_(session->service()),
+      timer_(session->service()),
+      timeout_(timeout),
+      socket_(Socket::Create(session->service())),
+      connected_(false),
+      buffer_size_(buffer_size) {}
+
+Connection::~Connection() {}
+
+void Connection::read() {
+    if (!connected_)
+        return;
+
+    buffer_in_.prepare(buffer_size_);
+    boost::asio::async_read(*socket_, buffer_in_,
+                            boost::asio::transfer_at_least(1),
+                            std::bind(&Connection::OnRead, this, std::placeholders::_1));
+}
+
+void Connection::write() {
+    if (!connected_)
+        return;
+
+    socket_->async_write_some(boost::asio::buffer(buffer_out_.data(),
+                                                  buffer_out_.size()),
+                              std::bind(&Connection::OnWritten, this, std::placeholders::_1));
+}
--- connection.h
+++ connection.h
@@ -5,18 +5,44 @@
 #include "common.h"
 #include "socket.h"
 
+class Session;
+
 class Connection : public std::enable_shared_from_this<Connection> {
 public:
-    virtual void start() = 0;
-    virtual void close() = 0;
-    virtual void reset() = 0;
+    virtual void read();
+    virtual void write();
+
+
+    boost::asio::streambuf& InboundBuffer() { return buffer_in_; }
+    boost::asio::streambuf& OutboundBuffer() { return buffer_out_; }
 
 protected:
-    DEFAULT_CTOR_AND_DTOR(Connection);
-    DISABLE_COPY_AND_ASSIGNMENT(Connection);
+    Connection(std::shared_ptr<Session> session,
+               long timeout = 30,
+               std::size_t buffer_size = 8192); // TODO
+    virtual ~Connection();
+
+    virtual void OnRead(const boost::system::error_code& e) = 0;
+    virtual void OnWritten(const boost::system::error_code& e) = 0;
+
+    virtual void ConstructMessage() = 0;
+
+protected:
+    std::weak_ptr<Session> session_;
+
+    boost::asio::io_service& service_;
+    boost::asio::deadline_timer timer_;
+    boost::posix_time::seconds timeout_;
+
+    std::unique_ptr<Socket> socket_;
+    bool connected_;
+
+    std::size_t buffer_size_;
+    boost::asio::streambuf buffer_in_;
+    boost::asio::streambuf buffer_out_;
 
 private:
-    std::unique_ptr<Socket> socket_;
+    DISABLE_COPY_AND_ASSIGNMENT(Connection);
 };
 
 #endif // CONNECTION_H
