926c03cdb44726d6ed376a94f5feaba7863719cb
Kelvin Hu <ini.kelvin@gmail.com>
2014-03-25 22:46:30 +0800

    update class Connection

--- connection.cpp
+++ connection.cpp
@@ -1,16 +1,23 @@
 #include "connection.h"
+#include "http_message.h"
+#include "log.h"
 #include "session.h"
 
 Connection::Connection(std::shared_ptr<Session> session,
+                       notify_callback&& complete_callback,
+                       notify_callback&& new_data_callback,
                        long timeout,
                        std::size_t buffer_size)
     : session_(session),
       service_(session->service()),
       timer_(session->service()),
       timeout_(timeout),
+      timer_triggered_(false),
       socket_(Socket::Create(session->service())),
       connected_(false),
-      buffer_size_(buffer_size) {}
+      buffer_size_(buffer_size),
+      new_data_callback_(std::forward(new_data_callback)),
+      complete_callback_(std::forward(complete_callback)) {}
 
 Connection::~Connection() {}
 
@@ -31,4 +38,47 @@ void Connection::write() {
     socket_->async_write_some(boost::asio::buffer(buffer_out_.data(),
                                                   buffer_out_.size()),
                               std::bind(&Connection::OnWritten, this, std::placeholders::_1));
+}
+
+void Connection::init() {
+    InitMessage();
+}
+
+void Connection::ConstructMessage() {
+    if (buffer_in_.size() <= 0)
+        return;
+
+    if (message_->completed()) {
+        XERROR_WITH_ID << "Message has been completed.";
+        return;
+    }
+
+    if (!message_->consume(buffer_in_)) {
+        XERROR_WITH_ID << "Parse failed.";
+        return;
+    }
+
+    if (new_data_callback_)
+        service_.post(std::bind(new_data_callback_, message_));
+
+    // TODO should we check if buffer_in_ is empty here?
+
+    if (!message_->completed()) {
+        read();
+        return;
+    }
+
+    if (complete_callback_)
+        service_.post(std::bind(complete_callback_, message_));
+}
+
+void Connection::reset() {
+    timer_triggered_ = false;
+    message_->reset();
+
+    if (buffer_in_.size() > 0)
+        buffer_in_.consume(buffer_in_.size());
+
+    if (buffer_out_.size() > 0)
+        buffer_out_.consume(buffer_out_.size());
 }
--- connection.h
+++ connection.h
@@ -5,34 +5,43 @@
 #include "common.h"
 #include "socket.h"
 
+class HttpMessage;
 class Session;
 
 class Connection : public std::enable_shared_from_this<Connection> {
 public:
+    typedef std::function<void(std::shared_ptr<HttpMessage>)> notify_callback;
+
     virtual void read();
     virtual void write();
 
-
-    boost::asio::streambuf& InboundBuffer() { return buffer_in_; }
-    boost::asio::streambuf& OutboundBuffer() { return buffer_out_; }
+    void init();
 
 protected:
     Connection(std::shared_ptr<Session> session,
+               notify_callback&& complete_callback,
+               notify_callback&& new_data_callback = notify_callback(),
                long timeout = 30,
                std::size_t buffer_size = 8192); // TODO
     virtual ~Connection();
 
     virtual void OnRead(const boost::system::error_code& e) = 0;
     virtual void OnWritten(const boost::system::error_code& e) = 0;
+    virtual void OnTimeout(const boost::system::error_code& e) = 0;
 
-    virtual void ConstructMessage() = 0;
+    virtual void InitMessage() = 0;
+    virtual void ConstructMessage();
+
+    void reset();
 
 protected:
     std::weak_ptr<Session> session_;
 
     boost::asio::io_service& service_;
+
     boost::asio::deadline_timer timer_;
     boost::posix_time::seconds timeout_;
+    bool timer_triggered_;
 
     std::unique_ptr<Socket> socket_;
     bool connected_;
@@ -40,6 +49,11 @@ protected:
     std::size_t buffer_size_;
     boost::asio::streambuf buffer_in_;
     boost::asio::streambuf buffer_out_;
+
+    std::shared_ptr<HttpMessage> message_;
+
+    notify_callback new_data_callback_; // when new data come
+    notify_callback complete_callback_; // when message is completed
 
 private:
     DISABLE_COPY_AND_ASSIGNMENT(Connection);
