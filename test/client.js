73a1e07663e800f9af9085a8359c8b5d0e692212
Kelvin Hu <ini.kelvin@gmail.com>
2014-03-27 22:53:58 +0800

    new test script

---
+++ test/client.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 7077;
+
+var socket = new net.Socket();
+socket.connect(PORT, HOST, function() {
+    console.log("Connected.");
+    socket.write('GET http://www.baidu.com HTTP/1.1\r\nHost: www.baidu.com\r\n\r\n');
+});
+
+socket.on('data', function(data) {
+    console.log('Response: ' + data);
+    socket.destroy();
+});
+
+socket.on('close', function() {
+    console.log('Connection closed.');
+});
