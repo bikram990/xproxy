239da5bb1eb2019c6a04ce76137cee9b7bc5d6d3
Kelvin Hu <ini.kelvin@gmail.com>
2014-04-18 00:49:30 +0800

    add basic unit test for SegmentalByteBuffer

---
+++ test/TestSegmentalByteBuffer.cpp
@@ -0,0 +1,34 @@
+#include "gtest/gtest.h"
+#include "memory/segmental_byte_buffer.hpp"
+
+TEST(SegmentalByteBufferTest, Common) {
+    SegmentalByteBuffer sbb;
+
+    EXPECT_TRUE(sbb.empty());
+    EXPECT_TRUE(sbb.size() == 0);
+    EXPECT_TRUE(sbb.SegmentCount() == 0);
+    EXPECT_TRUE(sbb.available() == 0);
+
+    sbb.append("abc", true);
+
+    EXPECT_FALSE(sbb.empty());
+    EXPECT_TRUE(sbb.size() == 3);
+    EXPECT_TRUE(sbb.SegmentCount() == 1);
+    EXPECT_TRUE(sbb.available() == 3);
+
+    sbb.consume(1);
+
+    EXPECT_TRUE(sbb.size() == 3);
+    EXPECT_TRUE(sbb.SegmentCount() == 1);
+    EXPECT_TRUE(sbb.available() == 2);
+
+    sbb.append("def", true);
+
+    EXPECT_TRUE(sbb.size() == 6);
+    EXPECT_TRUE(sbb.SegmentCount() == 2);
+    EXPECT_TRUE(sbb.available() == 5);
+
+    sbb.append("gh", false);
+
+    EXPECT_TRUE(sbb.SegmentCount() == 2);
+}
