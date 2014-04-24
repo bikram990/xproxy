e035b9f3771a76d05715adb61b0f84449d6bcf00
Kelvin Hu <ini.kelvin@gmail.com>
2014-04-24 21:48:10 +0800

    add unit test case for HttpParser

--- test/TestHttpParser.cpp
+++ test/TestHttpParser.cpp
@@ -1,7 +1,15 @@
 #include "CommonTestDefinition.hpp"
 #include "http_parser.hpp"
 
-TEST(HttpParserTest, Common) {
+TEST(HttpParserTest, Request) {
     std::shared_ptr<Connection> c;
     HttpParser parser(c, HTTP_REQUEST);
+
+    boost::asio::streambuf req1;
+    std::ostream out(&req1);
+    out << "GET http://github.com/kelvinh HTTP/1.1\r\n"
+        << "Host: github.com\r\n\r\n";
+
+    EXPECT_EQ(req1.size(), 60);
+    EXPECT_EQ(parser.consume(req1), req1.size());
 }
