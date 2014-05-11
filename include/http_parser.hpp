b3b272e2c49997c594f19dde56f2cc175a9407d8
Kelvin Hu <ini.kelvin@gmail.com>
2014-05-11 15:56:22 +0800

    update class HttpParser

--- include/http_parser.hpp
+++ include/http_parser.hpp
@@ -3,53 +3,30 @@
 
 #include "common.h"
 #include "http_parser.h"
-#include "http_message.hpp"
-#include "log.h"
 
-class Connection;
+class ConnectionAdapter;
+class HttpMessage;
 
 class HttpParser {
 public:
-    template<class Buffer>
-    std::size_t consume(const Buffer& buffer) {
-        auto orig_size = message_->RawBuffer().size();
-        message_->RawBuffer().append(boost::asio::buffer_cast<const char*>(buffer.data()), buffer.size(), false);
-
-        std::size_t consumed =
-            ::http_parser_execute(&parser_, &settings_,
-                                  //message_->RawBuffer().data(), message_->RawBuffer().available());
-                                  message_->RawBuffer().head() + orig_size, message_->RawBuffer().size() - orig_size);
-
-        if (consumed != message_->RawBuffer().available()) {
-            if (HTTP_PARSER_ERRNO(&parser_) != HPE_OK) {
-                XERROR << "Error occurred during message parsing, error code: "
-                       << parser_.http_errno << ", message: "
-                       << ::http_errno_description(static_cast<http_errno>(parser_.http_errno));
-                return 0;
-            } else {
-                // TODO will this happen? a message is parsed, but there is still data?
-                XWARN << "Weird: parser does not consume all data, but there is no error.";
-                return consumed;
-            }
-        }
-
-        return consumed;
-    }
+    std::size_t consume(const char* at, std::size_t length);
 
     bool HeaderCompleted() const { return header_completed_; }
 
     bool MessageCompleted() const { return message_completed_; }
-
-    HttpMessage& message() const { return *message_.get(); }
 
     bool KeepAlive() const;
 
     void reset();
 
 public:
-    HttpParser(std::shared_ptr<Connection> connection, http_parser_type type);
+    HttpParser(ConnectionAdapter *adapter, HttpMessage& message, http_parser_type type)
+        : adapter_(adapter), message_(message), header_completed_(false),
+          message_completed_(false), chunked_(false) {
+        ::http_parser_init(&parser_, type);
+    }
 
-    virtual ~HttpParser();
+    virtual ~HttpParser() = default;
 
     static int OnMessageBegin(http_parser *parser);
     static int OnUrl(http_parser *parser, const char *at, std::size_t length);
@@ -61,44 +38,16 @@ public:
     static int OnMessageComplete(http_parser *parser);
 
 private:
-    void InitMessage(http_parser_type type);
+    ConnectionAdapter *adapter_;
+    HttpMessage& message_;
 
-private:
-    struct buffer : public ByteBuffer::wrapper {
-        bool empty() const { return dt == nullptr; }
-        void clear() { dt = nullptr; sz = 0; }
-
-        void append(const char *data, std::size_t size) {
-            if (dt == nullptr) {
-                dt = data;
-                sz = size;
-                return;
-            }
-
-            // to make sure it is continuous
-            assert(data == dt + sz);
-
-            sz += size;
-        }
-
-        operator std::string() const {
-            return std::string(dt, sz);
-        }
-    };
-
-private:
-    std::weak_ptr<Connection> connection_;
-
-    http_parser parser_;
-
-    bool first_header_;
     bool header_completed_;
     bool message_completed_;
     bool chunked_;
-    buffer current_header_field_;
-    buffer current_header_value_;
+    std::string current_header_field_;
+    std::string current_header_value_;
 
-    std::unique_ptr<HttpMessage> message_;
+    http_parser parser_;
 
 private:
     static http_parser_settings settings_;
--- src/http_parser.cpp
+++ src/http_parser.cpp
@@ -1,7 +1,6 @@
-#include "connection.h"
+#include "connection_adapter.hpp"
+#include "http_message.hpp"
 #include "http_parser.hpp"
-#include "http_request.hpp"
-#include "http_response.hpp"
 
 http_parser_settings HttpParser::settings_ = {
     &HttpParser::OnMessageBegin,
@@ -14,20 +13,24 @@ http_parser_settings HttpParser::settings_ = {
     &HttpParser::OnMessageComplete
 };
 
-HttpParser::HttpParser(std::shared_ptr<Connection> connection,
-                       http_parser_type type)
-    : connection_(connection),
-      first_header_(true),
-      header_completed_(false),
-      message_completed_(false),
-      chunked_(false) {
-    ::http_parser_init(&parser_, type);
-    parser_.data = this;
+std::size_t HttpParser::consume(const char* at, std::size_t length) {
+    auto consumed = ::http_parser_execute(&parser_, &settings_, at, length);
 
-    InitMessage(type);
+    if (consumed != length) {
+        if (HTTP_PARSER_ERRNO(&parser_) != HPE_OK) {
+            XERROR << "Error occurred during message parsing, error code: "
+                   << parser_.http_errno << ", message: "
+                   << ::http_errno_description(static_cast<http_errno>(parser_.http_errno));
+            return 0;
+        } else {
+            // TODO will this happen? a message is parsed, but there is still data?
+            XWARN << "Weird: parser does not consume all data, but there is no error.";
+            return consumed;
+        }
+    }
+
+    return consumed;
 }
-
-HttpParser::~HttpParser() {}
 
 bool HttpParser::KeepAlive() const {
     if (!header_completed_) // return false when header is incomplete
@@ -42,57 +45,51 @@ void HttpParser::reset() {
     chunked_ = false;
     current_header_field_.clear();
     current_header_value_.clear();
-    message_->reset();
+    // message_.reset(); // TODO should we reset the message here?
 }
 
 int HttpParser::OnMessageBegin(http_parser *parser) {
-    HttpParser *p = static_cast<HttpParser*>(parser->data);
+    auto p = static_cast<HttpParser*>(parser->data);
     assert(p);
 
     p->header_completed_ = false;
     p->message_completed_ = false;
+    p->chunked_ = false;
+    p->current_header_field_.clear();
+    p->current_header_value_.clear();
     return 0;
 }
 
 int HttpParser::OnUrl(http_parser *parser, const char *at, std::size_t length) {
-    HttpParser *p = static_cast<HttpParser*>(parser->data);
+    auto p = static_cast<HttpParser*>(parser->data);
     assert(p);
 
     auto method = ::http_method_str(static_cast<http_method>(parser->method));
-    p->message_->SetField(HttpMessage::kRequestMethod,
-                          std::move(std::string(method)));
-    p->message_->SetField(HttpMessage::kRequestUri,
-                          std::move(std::string(at, length)));
+    p->message_.SetField(HttpMessage::kRequestMethod,
+                         std::move(std::string(method)));
+    p->message_.SetField(HttpMessage::kRequestUri,
+                         std::move(std::string(at, length)));
     return 0;
 }
 
 int HttpParser::OnStatus(http_parser *parser, const char *at, std::size_t length) {
-    HttpParser *p = static_cast<HttpParser*>(parser->data);
+    auto p = static_cast<HttpParser*>(parser->data);
     assert(p);
 
-    p->message_->SetField(HttpMessage::kResponseStatus,
-                          std::move(std::to_string(p->parser_.status_code)));
-    p->message_->SetField(HttpMessage::kResponseMessage,
-                          std::move(std::string(at, length)));
+    p->message_.SetField(HttpMessage::kResponseStatus,
+                         std::move(std::to_string(p->parser_.status_code)));
+    p->message_.SetField(HttpMessage::kResponseMessage,
+                         std::move(std::string(at, length)));
     return 0;
 }
 
 int HttpParser::OnHeaderField(http_parser *parser, const char *at, std::size_t length) {
-    HttpParser *p = static_cast<HttpParser*>(parser->data);
+    auto p = static_cast<HttpParser*>(parser->data);
     assert(p);
 
-    // we seperate the first line in buffer if it is the first header
-    if (p->first_header_) {
-        p->first_header_ = false;
-        // the length of the first line
-        auto length = std::distance(p->message_->RawBuffer().data(), at);
-        auto seg_end = p->message_->RawBuffer().seperate(length);
-        assert(seg_end == length);
-    }
-
     if (!p->current_header_value_.empty()) {
-        p->message_->AddHeader(p->current_header_field_,
-                               p->current_header_value_);
+        p->message_.AddHeader(p->current_header_field_,
+                              p->current_header_value_);
         p->current_header_field_.clear();
         p->current_header_value_.clear();
     }
@@ -101,7 +98,7 @@ int HttpParser::OnHeaderField(http_parser *parser, const char *at, std::size_t l
 }
 
 int HttpParser::OnHeaderValue(http_parser *parser, const char *at, std::size_t length) {
-    HttpParser *p = static_cast<HttpParser*>(parser->data);
+    auto p = static_cast<HttpParser*>(parser->data);
     assert(p);
 
     p->current_header_value_.append(at, length);
@@ -109,18 +106,11 @@ int HttpParser::OnHeaderValue(http_parser *parser, const char *at, std::size_t l
 }
 
 int HttpParser::OnHeadersComplete(http_parser *parser) {
-    HttpParser *p = static_cast<HttpParser*>(parser->data);
+    auto p = static_cast<HttpParser*>(parser->data);
     assert(p);
 
     // current_header_value_ MUST NOT be empty
     assert(!p->current_header_value_.empty());
-
-    // we seperate the headers segment in buffer here
-    // the last header end, and add the header trailing length: "\r\n\r\n"
-    auto headers_end = p->current_header_value_.data() + p->current_header_value_.size() + 4;
-    auto length = std::distance(p->message_->RawBuffer().data(), headers_end);
-    auto seg_end = p->message_->RawBuffer().seperate(length);
-    assert(seg_end == length);
 
     p->message_->AddHeader(p->current_header_field_,
                            p->current_header_value_);
@@ -132,18 +122,14 @@ int HttpParser::OnHeadersComplete(http_parser *parser) {
     if (p->parser_.flags & F_CHUNKED)
         p->chunked_ = true;
 
-    // TODO we need do more thing here, e.g. set the keep_alive flag
-    // TODO it's better to use io_service.post() here, and should
-    // wrap it with strand, same as below
-    auto c = p->connection_.lock();
-    if (c)
-        c->OnHeadersComplete();
+    if (adapter_)
+        adapter_->OnHeadersComplete();
 
     return 0;
 }
 
 int HttpParser::OnBody(http_parser *parser, const char *at, std::size_t length) {
-    HttpParser *p = static_cast<HttpParser*>(parser->data);
+    auto p = static_cast<HttpParser*>(parser->data);
     assert(p);
 
     if (p->chunked_) {
@@ -151,47 +137,31 @@ int HttpParser::OnBody(http_parser *parser, const char *at, std::size_t length) 
         out << std::hex << length;
         out.copyfmt(std::ios(nullptr));
         out << CRLF;
-        p->message_->AppendBody(out.str());
+        p->message_.AppendBody(out.str());
     }
-    p->message_->AppendBody(at, length, false);
+    p->message_.AppendBody(at, length);
     if (p->chunked_) {
-        p->message_->AppendBody(CRLF, 2, false);
+        p->message_.AppendBody(CRLF, 2);
     }
 
-    auto c = p->connection_.lock();
-    if (c)
-        c->OnBody();
+    if (adapter_)
+        adapter_->OnBody();
 
     return 0;
 }
 
 int HttpParser::OnMessageComplete(http_parser *parser) {
-    HttpParser *p = static_cast<HttpParser*>(parser->data);
+    auto p = static_cast<HttpParser*>(parser->data);
     assert(p);
 
     if (p->chunked_) {
-        p->message_->AppendBody(END_CHUNK, 5, false);
+        p->message_.AppendBody(END_CHUNK, 5);
     }
 
     p->message_completed_ = true;
 
-    auto c = p->connection_.lock();
-    if (c)
-        c->OnBodyComplete();
+    if (adapter_)
+        adapter_->OnBodyComplete();
 
     return 0;
-}
-
-void HttpParser::InitMessage(http_parser_type type) {
-    switch (type) {
-    case HTTP_REQUEST:
-        message_.reset(new HttpRequest);
-        break;
-    case HTTP_RESPONSE:
-        message_.reset(new HttpResponse);
-        break;
-    case HTTP_BOTH:
-    default:
-        ; // TODO ignore here?
-    }
 }
