883984398333ff13fbeb2a3789440866c5a41eba
Kelvin Hu <ini.kelvin@gmail.com>
2014-05-12 20:26:02 +0800

    update ByteBuffer

--- include/memory/byte_buffer.hpp
+++ include/memory/byte_buffer.hpp
@@ -3,16 +3,16 @@
 
 #include <string>
 
+namespace xproxy {
+namespace memory {
+
 /**
  * @brief The ByteBuffer class
  *
  * The ByteBuffer is a buffer for binary content.
- *
  */
 class ByteBuffer {
 public:
-    friend class SegmentalByteBuffer;
-
     typedef std::size_t size_type;
     typedef char* pointer_type;
     typedef const char* const_pointer_type;
@@ -28,15 +28,12 @@ public:
     enum { kDefaultSize = 1024, kGrowFactor = 2 };
 
     struct wrapper {
-        const char *dt;
-        std::size_t sz;
-
-        const char *data() const { return dt; }
-        std::size_t size() const { return sz; }
+        const_pointer_type data;
+        size_type size;
     };
 
-    static wrapper wrap(const void *data, std::size_t size) {
-        return wrapper{static_cast<const char*>(data), size};
+    static wrapper wrap(const_pointer_type data, size_type size) {
+        return wrapper{data, size};
     }
 
     virtual ~ByteBuffer() {
@@ -48,12 +45,19 @@ public:
           capacity_(size ? size : kDefaultSize) {}
 
     ByteBuffer(const void *data, size_type size) : ByteBuffer(size) {
-        // TODO verify data here: if(!data) blahblahblah...
+        assert(data);
         std::memcpy(data_, data, size);
         size_ = size;
     }
 
     ByteBuffer(const ByteBuffer& buffer) : ByteBuffer(buffer.data_, buffer.size_) {}
+
+    ByteBuffer(ByteBuffer&& buffer)
+        : data_(buffer.data_), size_(buffer.size_), capacity_(buffer.capacity_) {
+        buffer.data_ = nullptr;
+        buffer.size_ = 0;
+        buffer.capacity_ = 0;
+    }
 
     ByteBuffer& operator=(const ByteBuffer& buffer) {
         if(this->capacity_ < buffer.size_) {
@@ -64,6 +68,19 @@ public:
 
         std::memcpy(data_, buffer.data_, buffer.size_);
         size_ = buffer.size_;
+
+        return *this;
+    }
+
+    ByteBuffer& operator=(ByteBuffer&& buffer) {
+        delete [] data_;
+
+        data_ = buffer.data_;
+        size_ = buffer.size_;
+        capacity_ = buffer.capacity_;
+        buffer.data_ = nullptr;
+        buffer.size_ = 0;
+        buffer.capacity_ = 0;
 
         return *this;
     }
@@ -84,54 +101,55 @@ public:
     }
 
     ByteBuffer& operator<<(char c) {
-        EnsureSize(1);
+        ensureSize(1);
         data_[size_++] = c;
         return *this;
     }
 
     ByteBuffer& operator<<(const std::string& str) {
-        EnsureSize(str.size());
-        for(auto it = str.begin(); it != str.end(); ++it) {
+        ensureSize(str.size());
+        for (auto it : str) {
             data_[size_++] = *it;
         }
         return *this;
     }
 
     ByteBuffer& operator<<(const ByteBuffer& buffer) {
-        EnsureSize(buffer.size());
+        ensureSize(buffer.size());
         std::memcpy(data_ + size_, buffer.data(), buffer.size());
         size_ += buffer.size();
         return *this;
     }
 
-    ByteBuffer& operator<<(const_pointer_type data) {
-        return operator<<(std::string{data});
+    ByteBuffer& operator<<(const wrapper& w) {
+        ensureSize(w.size);
+        std::memcpy(data_ + size_, w.data, w.size);
+        size_ += w.size;
+        return *this;
     }
 
     ByteBuffer& operator<<(int num) {
         return operator<<(std::to_string(num));
     }
 
-    template<class Buffer>
-    ByteBuffer& operator<<(const Buffer& buffer) {
-        EnsureSize(buffer.size());
-        std::memcpy(data_ + size_, buffer.data(), buffer.size());
-        size_ += buffer.size();
+    template<typename ContinuousByteSequence>
+    ByteBuffer& operator<<(const ContinuousByteSequence& sequence) {
+        ensureSize(sequence.size());
+        std::memcpy(data_ + size_, sequence.data(), sequence.size());
+        size_ += sequence.size();
         return *this;
     }
 
-    pointer_type   	data()           { return data_; }
+    pointer_type   	   data()           { return data_; }
     const_pointer_type data()     const { return data_; }
-    size_type      	size()     const { return size_; }
-    bool           	empty()    const { return size_ == 0; }
-    size_type      	capacity() const { return capacity_; }
-    iterator       	begin()          { return data_; }
-    const_iterator 	begin()    const { return data_; }
-    iterator       	end()            { return data_ + size_; }
-    const_iterator 	end()      const { return data_ + size_; }
-
-    void clear() { size_ = 0; }
-    void reset() { size_ = 0; } // TODO we may do shrink job here
+    iterator       	   begin()          { return data_; }
+    const_iterator 	   begin()    const { return data_; }
+    iterator       	   end()            { return data_ + size_; }
+    const_iterator 	   end()      const { return data_ + size_; }
+    size_type      	   size()     const { return size_; }
+    bool           	   empty()    const { return size_ == 0; }
+    size_type      	   capacity() const { return capacity_; }
+    void               clear()          { size_ = 0; } // TODO shrink job here?
 
     pointer_type data(size_type pos) {
         return pos < size_ ? data_ + pos : nullptr;
@@ -142,8 +160,8 @@ public:
     }
 
 private:
-    void EnsureSize(size_type size) {
-        if(size_ + size <= capacity_)
+    void ensureSize(size_type size) {
+        if (size_ + size <= capacity_)
             return;
 
         char *tmp = data_;
@@ -158,5 +176,8 @@ private:
     size_type size_;
     size_type capacity_;
 };
+
+}
+}
 
 #endif // BYTE_BUFFER_HPP
