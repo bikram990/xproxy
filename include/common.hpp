727a0060e398c03d2a9e0926187d5d5e003f5f19
Kelvin Hu <ini.kelvin@gmail.com>
2014-05-12 19:18:55 +0800

    new common header

---
+++ include/common.hpp
@@ -0,0 +1,33 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#define DEFAULT_CTOR_AND_DTOR(T) T() = default; \
+    virtual ~T() = default
+
+#define DEFAULT_DTOR(T) ~T() = default
+
+#define DEFAULT_VIRTUAL_DTOR(T) virtual ~T() = default
+
+#define MAKE_NONCOPYABLE(T) T(const T&) = delete; \
+    T& operator=(const T&) = delete
+
+
+/*
+ * Some handy definitions for HTTP message processing.
+ */
+#define CR '\r'
+#define LF '\n'
+#define CRLF "\r\n"
+#define END_CHUNK "0\r\n\r\n"
+
+/*
+ * Short read error in SSL mode.
+ *
+ * The actual code is 335544539, for how is the code generated, see function
+ * engine::map_error_code(...) in asio/ssl/detail/impl/engine.ipp.
+ *
+ * When we get short read error, we should treat it as EOF in normal mode.
+ */
+#define SSL_SHORT_READ(ec) ((ec.value() & 0xFF) == SSL_R_SHORT_READ)
+
+#endif // COMMON_HPP
