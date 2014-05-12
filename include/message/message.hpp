eabead19e447e0c6ea331d63a543f12de0ff4c8c
Kelvin Hu <ini.kelvin@gmail.com>
2014-05-12 20:28:50 +0800

    new interface: Message

---
+++ include/message/message.hpp
@@ -0,0 +1,23 @@
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
+
+namespace xproxy {
+namespace memory { class ByteBuffer; }
+namespace message {
+
+/**
+ * @brief The Message class
+ *
+ * The generic message interface.
+ */
+class Message {
+public:
+    virtual ~Message() {}
+
+    virtual std::size_t serialize(xproxy::memory::ByteBuffer& buffer) = 0;
+};
+
+}
+}
+
+#endif // MESSAGE_HPP
