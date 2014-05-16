d6921a9c84b9beeef52665ed50e6f5de679885d6
Kelvin Hu <ini.kelvin@gmail.com>
2014-05-16 14:57:42 +0800

    new Counter class

---
+++ include/util/counter.hpp
@@ -0,0 +1,37 @@
+#ifndef COUNTER_HPP
+#define COUNTER_HPP
+
+#include <atomic>
+#include "common.hpp"
+
+namespace xproxy {
+namespace util {
+
+template<class T>
+class Counter {
+public:
+    std::size_t id() const { return id_; }
+
+protected:
+    Counter() : id_(++counter_) {}
+
+    DEFAULT_VIRTUAL_DTOR(Counter);
+
+private:
+    std::size_t id_;
+
+    static std::atomic<std::size_t> counter_;
+
+private:
+    // TODO a better solution, is to increase id_ when copying,
+    // but not just disable copying rudely
+    MAKE_NONCOPYABLE(Counter);
+};
+
+template<class T>
+std::atomic<std::size_t> Counter<T>::counter_(0);
+
+} // namespace util
+} // namespace xproxy
+
+#endif // COUNTER_HPP
