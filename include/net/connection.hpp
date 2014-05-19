93ef039193cbd469b555a2da39d23efa02094001
Kelvin Hu <ini.kelvin@gmail.com>
2014-05-19 21:41:05 +0800

    update Connection class and its dependency classes

--- include/net/connection.hpp
+++ include/net/connection.hpp
@@ -8,6 +8,8 @@
 #include <vector>
 #include <boost/asio.hpp>
 #include "common.hpp"
+#include "resource_manager.h"
+#include "util/counter.hpp"
 
 namespace xproxy {
 namespace memory { class ByteBuffer; }
@@ -23,24 +25,48 @@ public:
     virtual void onConnect(const boost::system::error_code& e) = 0;
     virtual void onHandshake(const boost::system::error_code& e) = 0;
     virtual void onRead(const boost::system::error_code& e, const char *data, std::size_t length) = 0;
-    virtual void onWrite(const boost::system::error_code& e, xproxy::memory::ByteBuffer& buffer, std::size_t length) = 0;
+    virtual void onWrite(const boost::system::error_code& e) = 0;
 };
 
-class Connection : public std::enable_shared_from_this<Connection> {
+struct ConnectionContext {
+    ConnectionContext() : https(false), proxied(false) {}
+
+    bool https;
+    bool proxied;
+    std::string remote_host;
+    std::string remote_port;
+};
+
+typedef std::shared_ptr<ConnectionContext> SharedConnectionContext;
+
+class Connection : public util::Counter<Connection>, public std::enable_shared_from_this<Connection> {
 public:
     // typedef std::vector<char> buffer_type;
     typedef xproxy::memory::ByteBuffer buffer_type;
+
+    boost::asio::io_service& service() const { return service_; }
+
+    void closeSocket();
 
     virtual void start() = 0;
     virtual void stop() = 0;
 
     virtual void connect(const std::string& host, const std::string& port) = 0;
-    virtual void handshake() = 0;
-    virtual void read() = 0;
-    virtual void write(const xproxy::message::Message& message) = 0;
+    virtual void handshake(ResourceManager::CertManager::CAPtr ca = nullptr,
+                           ResourceManager::CertManager::DHParametersPtr dh = nullptr) = 0;
+    virtual void read();
+    virtual void write(const xproxy::message::Message& message);
+    virtual void write(const std::string& str);
+
+    virtual ConnectionPtr bridgeConnection() = 0;
 
 protected:
-    virtual void doWrite() = 0;
+    Connection(boost::asio::io_service& service,
+               SharedConnectionContext context);
+    DEFAULT_VIRTUAL_DTOR(Connection);
+
+protected:
+    virtual void doWrite();
     virtual void doConnect() = 0;
 
 private:
@@ -50,9 +76,35 @@ private:
     std::unique_ptr<SocketFacade> socket_;
 
     std::array<char, kBufferSize> buffer_in_;
-    std::list<buffer_type> buffer_out_;
+    std::list<std::shared_ptr<buffer_type>> buffer_out_;
 
     std::unique_ptr<ConnectionAdapter> adapter_;
+
+    SharedConnectionContext context_;
+};
+
+class ClientConnection : public Connection {
+public:
+    virtual void start();
+    virtual void stop();
+    virtual void connect(const std::string& host, const std::string& port);
+    virtual void handshake(ResourceManager::CertManager::CAPtr ca, ResourceManager::CertManager::DHParametersPtr dh);
+    virtual ConnectionPtr bridgeConnection();
+
+protected:
+    virtual void doConnect();
+};
+
+class ServerConnection : public Connection {
+public:
+    virtual void start();
+    virtual void stop();
+    virtual void connect(const std::string& host, const std::string& port);
+    virtual void handshake(ResourceManager::CertManager::CAPtr ca, ResourceManager::CertManager::DHParametersPtr dh);
+    virtual ConnectionPtr bridgeConnection();
+
+protected:
+    virtual void doConnect();
 };
 
 class ConnectionManager {
--- src/connection.cpp
+++ src/connection.cpp
@@ -1,7 +1,71 @@
+#include "log.h"
+#include "message/http/http_message.hpp"
 #include "net/connection.hpp"
 
 namespace xproxy {
 namespace net {
+
+void Connection::closeSocket() {
+    if (socket_)
+        socket_->close();
+}
+
+void Connection::read() {
+    auto self(shared_from_this());
+    boost::asio::async_read(*socket,
+                            buffer_in_,
+                            boost::asio::transfer_at_least(1),
+                            [self, this](const boost::system::error_code& e, std::size_t length) {
+        if (adapter_)
+            adapter_->onRead(e, buffer_in_.data(), length);
+    });
+}
+
+void Connection::write(const message::Message &message) {
+    std::shared_ptr<memory::ByteBuffer> buf(new memory::ByteBuffer);
+    auto size = message.serialize(*buf);
+    if (size <= 0) {
+        XERROR_WITH_ID << "Write, no content.";
+        return;
+    }
+
+    buffer_out_.push_back(buf);
+
+    doWrite();
+}
+
+void Connection::write(const std::string& str) {
+    std::shared_ptr<memory::ByteBuffer> buf(new memory::ByteBuffer);
+    *buf << str;
+    buffer_out_.push_back(buf);
+
+    doWrite();
+}
+
+void Connection::doWrite() {
+    auto& candidate = buf_out_.front();
+    auto buf = boost::asio::buffer(candidate->data(), candidate->size());
+    auto self(shared_from_this());
+    socket_->async_write_some(buf,
+                              [self, this](const boost::system::error_code& e, std::size_t length) {
+        if (length < candidate->size()) {
+            XWARN_WITH_ID << "Write incomplete, continue.";
+            candidate->erase(0, length);
+            doWrite();
+            return;
+        }
+
+        buffer_out_.erase(buffer_out_.begin());
+        if (!buffer_out_.empty()) {
+            XDEBUG_WITH_ID << "More buffers added, continue writing.";
+            doWrite();
+            return;
+        }
+
+        if (adapter_)
+            adapter_->onWrite(e);
+    });
+}
 
 void ConnectionManager::start(ConnectionPtr &connection) {
     connections_.insert(connection);
