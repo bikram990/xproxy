0f1e4433db618a62317e35051f505bf3333efe0b
Kelvin Hu <ini.kelvin@gmail.com>
2014-05-21 10:48:28 +0800

    fix error caused by ClientAdapter

--- src/client_adapter.cpp
+++ src/client_adapter.cpp
@@ -1,4 +1,5 @@
-#include "log.h"
+#include <boost/asio/ssl.hpp>
+#include "log/log.hpp"
 #include "memory/byte_buffer.hpp"
 #include "message/http/http_request.hpp"
 #include "net/client_adapter.hpp"
@@ -20,7 +21,7 @@ void ClientAdapter::onConnect(const boost::system::error_code& e) {
 
 void ClientAdapter::onHandshake(const boost::system::error_code& e) {
     if (e) {
-        XERROR_WITH_ID << "Handshake error: " << e.message();
+        XERROR_ID_WITH(connection_) << "Handshake error: " << e.message();
         // TODO enhance
         connection_.stop();
         return;
@@ -35,26 +36,26 @@ void ClientAdapter::onHandshake(const boost::system::error_code& e) {
 void ClientAdapter::onRead(const boost::system::error_code &e, const char *data, std::size_t length) {
     if (e) {
         if(e == boost::asio::error::eof || SSL_SHORT_READ(e))
-            XDEBUG_WITH_ID << "EOF in socket, stop.";
+            XDEBUG_ID_WITH(connection_) << "EOF in socket, stop.";
         else
-            XERROR_WITH_ID << "Read error: " << e.message();
+            XERROR_ID_WITH(connection_) << "Read error: " << e.message();
         // TODO enhance
         connection_.stop();
         return;
     }
 
     if (length <= 0) {
-        XWARN_WITH_ID << "Read, no data.";
+        XWARN_ID_WITH(connection_) << "Read, no data.";
         // TODO enhance
         connection_.stop();
         return;
     }
 
-    XDEBUG_WITH_ID << "Read, length: " << length << ", content:\n"
-                   << std::string(data, length);
+    XDEBUG_ID_WITH(connection_) << "Read, length: " << length << ", content:\n"
+                                << std::string(data, length);
 
-    if (parser_->MessageCompleted()) {
-        XERROR_WITH_ID << "Read, message is already completed.";
+    if (parser_->messageCompleted()) {
+        XERROR_ID_WITH(connection_) << "Read, message is already completed.";
         // TODO enhance
         connection_.stop();
         return;
@@ -62,32 +63,32 @@ void ClientAdapter::onRead(const boost::system::error_code &e, const char *data,
 
     auto consumed = parser_->consume(data, length);
     if (consumed <= 0) {
-        XERROR_WITH_ID << "Read, message parsing failure.";
+        XERROR_ID_WITH(connection_) << "Read, message parsing failure.";
         // TODO enhance
         connection_.stop();
         return;
     }
 
-    if (!parser_->MessageCompleted())
+    if (!parser_->messageCompleted())
         connection_.read();
 }
 
 void ClientAdapter::onWrite(const boost::system::error_code& e) {
     if (e) {
-        XERROR_WITH_ID << "Write error: " << e.message();
+        XERROR_ID_WITH(connection_) << "Write error: " << e.message();
         connection_.stop();
         return;
     }
 
     if (https_ && !ssl_built_) {
-        XDEBUG_WITH_ID << "SSL reply written.";
+        XDEBUG_ID_WITH(connection_) << "SSL reply written.";
         auto ca = ResourceManager::GetCertManager().GetCertificate(remote_host_);
         auto dh = ResourceManager::GetCertManager().GetDHParameters();
         connection_.handshake(ca, dh);
         return;
     }
 
-    XDEBUG_WITH_ID << "Response data written.";
+    XDEBUG_ID_WITH(connection_) << "Response data written.";
 }
 
 void ClientAdapter::onHeadersComplete(message::http::HttpMessage&) {
@@ -101,7 +102,7 @@ void ClientAdapter::onBody(message::http::HttpMessage&) {
 void ClientAdapter::onMessageComplete(message::http::HttpMessage& message) {
     if (!https_) { // a normal HTTP request or a CONNECT request
         if (!ParseRemotePeer()) {
-            XERROR_WITH_ID << "Remote host/port parsing failure.";
+            XERROR_ID_WITH(connection_) << "Remote host/port parsing failure.";
             // TODO enhance
             connection_.stop();
             return;
@@ -118,9 +119,9 @@ void ClientAdapter::onMessageComplete(message::http::HttpMessage& message) {
         return;
     }
 
-    auto bridge = connection_.bridgeConnection();
+    auto bridge = connection_.getBridgeConnection();
     if (!bridge) {
-        XERROR_WITH_ID << "No bridge connection.";
+        XERROR_ID_WITH(connection_) << "No bridge connection.";
         // TODO enhance
         connection_.stop();
         return;
@@ -130,25 +131,25 @@ void ClientAdapter::onMessageComplete(message::http::HttpMessage& message) {
 }
 
 bool ClientAdapter::ParseRemotePeer() {
-    if (!parser_->HeadersCompleted()) {
-        XERROR_WITH_ID << "Incomplete header.";
+    if (!parser_->headersCompleted()) {
+        XERROR_ID_WITH(connection_) << "Incomplete header.";
         return false;
     }
 
     auto method = message_->getField(message::http::HttpMessage::kRequestMethod);
     if (method.length() == 7 && method[0] == 'C' && method[1] == 'O') {
         remote_host_ = message_->getField(message::http::HttpMessage::kRequestUri);
-        remote_port_ = 443;
+        remote_port_ = "443";
 
-        auto sep = host_.find(':');
+        auto sep = remote_host_.find(':');
         if (sep != std::string::npos) {
             remote_port_ = remote_host_.substr(sep + 1);
             remote_host_ = remote_host_.substr(0, sep);
         }
     } else {
-        remote_port_ = 80;
+        remote_port_ = "80";
         if (!message_->findHeader("Host", remote_host_)) {
-            XERROR_WITH_ID << "Header \"Host\" not found.";
+            XERROR_ID_WITH(connection_) << "Header \"Host\" not found.";
             return false;
         }
 
