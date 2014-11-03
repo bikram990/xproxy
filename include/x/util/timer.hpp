3e56cfaa711f4d513c6f16b3dad35405b419902b
Kelvin Hu <ini.kelvin@gmail.com>
2014-11-04 01:17:08 +0800

    refine naming

--- include/x/util/timer.hpp
+++ include/x/util/timer.hpp
@@ -1,52 +1,45 @@
 #ifndef TIMER_HPP
 #define TIMER_HPP
 
-#include <boost/asio.hpp>
-#include "xproxy/log/log.hpp"
-
-namespace xproxy {
+namespace x {
 namespace util {
 
-/**
- * @brief The Timer class
- *
- * This class is a simple wrapper for timer in boost asio.
- */
-class Timer {
+class timer {
 public:
-    Timer(boost::asio::io_service& service)
+    timer(boost::asio::io_service& service)
         : timer_(service),
           running_(false),
           triggered_(false) {}
 
-    ~Timer() = default;
+    DEFAULT_DTOR(timer);
 
     bool running() const { return running_; }
     bool triggered() const { return triggered_; }
 
     template<typename TimeoutHandler>
     void start(long timeout, TimeoutHandler&& handler) {
-        if (running_)
-            return;
+        if (running_) return;
+
         timer_.expires_from_now(boost::posix_time::seconds(timeout));
         timer_.async_wait([this, handler] (const boost::system::error_code& e) {
             running_ = false;
 
             if (e == boost::asio::error::operation_aborted)
-                XDEBUG << "Timer cancelled.";
+                XDEBUG << "timer cancelled.";
             else if (e)
-                XERROR << "Timer error, message: " << e.message();
+                XERROR << "timer error, code:" << e.value()
+                       << ", message: " << e.message();
             else if (timer_.expires_at() <= boost::asio::deadline_timer::traits_type::now()) {
                 triggered_ = true;
                 handler(e);
             }
         });
+
         running_ = true;
     }
 
     void cancel() {
-        if (running_)
-            timer_.cancel();
+        if (running_) timer_.cancel();
     }
 
 private:
@@ -56,6 +49,6 @@ private:
 };
 
 } // namespace util
-} // namespace xproxy
+} // namespace x
 
 #endif // TIMER_HPP
