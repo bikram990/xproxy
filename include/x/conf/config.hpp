32fa833a0b9c6ad5537e860ee8c5ca1cf13ee46a
Kelvin Hu <ini.kelvin@gmail.com>
2014-11-04 00:18:15 +0800

    refine naming

--- include/x/conf/config.hpp
+++ include/x/conf/config.hpp
@@ -6,13 +6,23 @@ namespace conf {
 
 class config {
 public:
-    config(const std::string& conf_file = "xproxy.conf") {
-        if(!load_config(conf_file)) {
-            #warning throw an exception here?
-        }
-    }
+    config(const std::string& conf_file = "xproxy.conf")
+        : conf_file_(conf_file) {}
 
     DEFAULT_DTOR(config);
+
+    bool load_config() {
+        try {
+            boost::property_tree::ini_parser::read_ini(conf_file_, conf_tree_);
+        } catch(boost::property_tree::ini_parser::ini_parser_error& e) {
+            XFATAL << "load config error, path: " << conf_file
+                   << ", reason: " << e.what();
+            return false;
+        }
+
+        XINFO << "config file " << conf_file << " loaded.";
+        return true;
+    }
 
     template<typename T>
     bool get_config(const std::string& path, T& return_value) {
@@ -27,19 +37,7 @@ public:
     }
 
 private:
-    bool load_config(const std::string& conf_file) {
-        try {
-            boost::property_tree::ini_parser::read_ini(conf_file, conf_tree_);
-        } catch(boost::property_tree::ini_parser::ini_parser_error& e) {
-            XFATAL << "load config error, path: " << conf_file
-                   << ", reason: " << e.what();
-            return false;
-        }
-
-        XINFO << "config file " << conf_file << " loaded.";
-        return true;
-    }
-
+    std::string conf_file_;
     boost::property_tree::ptree conf_tree_;
 };
 
